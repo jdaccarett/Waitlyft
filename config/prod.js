42af7a850bced25e9d8cf9f8068266123761fadc
jdaccarett <jdaccarett101@gmail.com>
2018-02-27 18:47:24 -0800

    finished auth flow

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,10 @@
 // prod.js - production keys here.
-module.exports = {
-  facebookClientID : 417821688671161,
-  facebookClientSecret: "94b0aee63a4031e25414ec3e55a24cab",
-  mongoURI: 'mongodb://jdaccarett:weeman91@ds251548.mlab.com:51548/waitlyft-prod',
-  cookieKey: 'sdjfsldjfsldjfsdhgsdfjsldfjsttsdfksdd'
-};
+// module.exports = {
+//   facebookClientID : 417821688671161,
+//   facebookClientSecret: "94b0aee63a4031e25414ec3e55a24cab",
+//   mongoURI: 'mongodb://jdaccarett:weeman91@ds251548.mlab.com:51548/waitlyft-prod',
+//   cookieKey: 'sdjfsldjfsldjfsdhgsdfjsldfjsttsdfksdd'
+// };
 
 // prod.js - production keys here.
 module.exports = {
